98c691d6fbfd6a7253cdc299cd2c3cd767aadaa9
gilang <lintang.gilang01@gmail.com>
2020-07-06 21:16:34 +0700

    penambahan latian JS

---
+++ es6_study_3/page4/script.js
@@ -0,0 +1,8 @@
+// Tambahkan parameter name ke function
+const greet = (name) => {
+  // Print pesan "Halo, ____"
+  console.log(`halo, ${name}`);
+};
+
+// Panggil function greet dengan "Guru Domba" sebagai argument
+greet("guru domba");
